a7100701bbee54959f2846593a7588a132d43ebf
fainting-goat <kelsey.shannahan@gmail.com>
2018-12-29 21:23:07 -0500

    stupid stupid stupid

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :customizer, CustomizerWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "localhost", port: 4012],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["http://wolfhoundcustomizer.com/, https://wolfhoundcustomizer.com/, http://www.wolfhoundcustomizer.com/, https://www.wolfhoundcustomizer.com/"]
+  check_origin: ["http://wolfhoundcustomizer.com/", "https://wolfhoundcustomizer.com/", "http://www.wolfhoundcustomizer.com/", "https://www.wolfhoundcustomizer.com/"]
 
 # Do not print debug messages in production
 config :logger, level: :info
